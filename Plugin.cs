5002cd752af0404d314079fe1831207ba86a6c82
Myles Rockland <90970277+myles-rockland@users.noreply.github.com>
2026-04-06 22:54:01 +0100

    Create BepInEx5 template plugin project

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace ShovelKnightDigAP
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            // Make a Harmony instance, patch all the methods
+
+            // Not sure if manual patching or automatic patching with attributes is better?
+        }
+    }
+}
---
+++ lib/lib.md
@@ -0,0 +1,9 @@
+# Required files
+
+The following files are required to build the project.
+
+These files are obtainable from Shovel Knight Dig's install path, under `<install path>/skDig64_Data/Managed/`
+
+- Assembly-CSharp.dll
+- UnityEngine.dll
+- UnityEngine.CoreModule.dll
