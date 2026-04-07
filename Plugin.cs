c6991149fd8dd1ca752c5a3cc878756ffc0912c5
Myles Rockland <90970277+myles-rockland@users.noreply.github.com>
2026-04-07 20:33:08 +0100

    Regenerate project with AP template

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,74 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using ShovelKnightDigAP.Archipelago;
+using ShovelKnightDigAP.Utils;
 using UnityEngine;
 
-namespace ShovelKnightDigAP
+namespace ShovelKnightDigAP;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    public const string PluginGUID = "com.rockm.shovelknightdigap";
+    public const string PluginName = "ShovelKnightDigAP";
+    public const string PluginVersion = "0.0.0";
+
+    public const string ModDisplayInfo = $"{PluginName} v{PluginVersion}";
+    private const string APDisplayInfo = $"Archipelago v{ArchipelagoClient.APVersion}";
+    public static ManualLogSource BepinLogger;
+    public static ArchipelagoClient ArchipelagoClient;
+
+    private void Awake()
     {
-        internal static new ManualLogSource Logger;
+        // Plugin startup logic
+        BepinLogger = Logger;
+        ArchipelagoClient = new ArchipelagoClient();
+        ArchipelagoConsole.Awake();
 
-        private void Awake()
+        ArchipelagoConsole.LogMessage($"{ModDisplayInfo} loaded!");
+    }
+
+    private void OnGUI()
+    {
+        // show the mod is currently loaded in the corner
+        GUI.Label(new Rect(16, 16, 300, 20), ModDisplayInfo);
+        ArchipelagoConsole.OnGUI();
+
+        string statusMessage;
+        // show the Archipelago Version and whether we're connected or not
+        if (ArchipelagoClient.Authenticated)
         {
-            // Plugin startup logic
-            Logger = base.Logger;
-            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            // if your game doesn't usually show the cursor this line may be necessary
+            // Cursor.visible = false;
 
-            // Make a Harmony instance, patch all the methods
-
-            // Not sure if manual patching or automatic patching with attributes is better?
+            statusMessage = " Status: Connected";
+            GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
         }
+        else
+        {
+            // if your game doesn't usually show the cursor this line may be necessary
+            // Cursor.visible = true;
+
+            statusMessage = " Status: Disconnected";
+            GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
+            GUI.Label(new Rect(16, 70, 150, 20), "Host: ");
+            GUI.Label(new Rect(16, 90, 150, 20), "Player Name: ");
+            GUI.Label(new Rect(16, 110, 150, 20), "Password: ");
+
+            ArchipelagoClient.ServerData.Uri = GUI.TextField(new Rect(150, 70, 150, 20),
+                ArchipelagoClient.ServerData.Uri);
+            ArchipelagoClient.ServerData.SlotName = GUI.TextField(new Rect(150, 90, 150, 20),
+                ArchipelagoClient.ServerData.SlotName);
+            ArchipelagoClient.ServerData.Password = GUI.TextField(new Rect(150, 110, 150, 20),
+                ArchipelagoClient.ServerData.Password);
+
+            // requires that the player at least puts *something* in the slot name
+            if (GUI.Button(new Rect(16, 130, 100, 20), "Connect") &&
+                !ArchipelagoClient.ServerData.SlotName.IsNullOrWhiteSpace())
+            {
+                ArchipelagoClient.Connect();
+            }
+        }
+        // this is a good place to create and add a bunch of debug buttons
     }
 }
