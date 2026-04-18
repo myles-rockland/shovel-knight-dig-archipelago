ebf0dc6797ebcfbd3ac2237968f844b5ef2998de
Myles Rockland <90970277+myles-rockland@users.noreply.github.com>
2026-04-18 11:47:24 +0100

    Add InventoryPatch to clear initial unlocks

--- Patches/CollectiblePatch.cs
+++ Patches/CollectiblePatch.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using HarmonyLib;
-using ShovelKnightDigAPClient.Archipelago;
+﻿using HarmonyLib;
+using System;
 
 namespace ShovelKnightDigAPClient.Patches
 {
     [HarmonyPatch(typeof(Collectible))]
     public class CollectiblePatch
     {
+        public static Action<Collectible> onCollectibleCollected;
+
         [HarmonyPatch(nameof(Collectible.Collect))]
         [HarmonyPostfix]
         public static void CheckCog(Collectible __instance)
@@ -17,6 +16,16 @@ namespace ShovelKnightDigAPClient.Patches
             {
                 Plugin.BepinLogger.LogMessage($"Cog {__instance.GetComponent<GearCollectible>().ProgressionIndex} collected in {StageController.Theme.ToString()}!");
                 //Plugin.ArchipelagoClient.SendMessage("Gear Collected");
+
+                // Loop through permanent items, when you find the fire ring, add it via Inventory.Player1Inventory.AddPermItem(item);
+                foreach (var item in Inventory.Player1Inventory.m_PermanentUpgradeItems)
+                {
+                    if (item.m_ID == "FIRE_RING")
+                    {
+                        Inventory.Player1Inventory.AddPermItem(item);
+                    }
+                    break;
+                }
             }
         }
     }
---
+++ Patches/InventoryPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+
+namespace ShovelKnightDigAPClient.Patches
+{
+    [HarmonyPatch(typeof(Inventory))]
+    public class InventoryPatch
+    {
+        [HarmonyPatch(nameof(Inventory.Init))]
+        [HarmonyPrefix]
+        public static void ClearInitialBuffs(Inventory __instance)
+        {
+            Plugin.BepinLogger.LogMessage("Resizing m_InitialUnlockedBuffs to 0!");
+            Array.Resize(ref __instance.m_InitialUnlockedBuffs, 0);
+        }
+    }
+}
