ef679d6ffca869ba460c1f84ce72df34246ed0f2
Myles Rockland <90970277+myles-rockland@users.noreply.github.com>
2026-04-19 21:14:41 +0100

    Add OwlPatch

---
+++ Patches/OwlPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace ShovelKnightDigAPClient.Patches
+{
+    [HarmonyPatch(typeof(Owl))]
+    public class OwlPatch
+    {
+        [HarmonyPatch(nameof(Owl.InitInOverworld))]
+        [HarmonyPostfix]
+        public static void DeactivateOwlIfNotUnlocked(Owl __instance)
+        {
+            if (SaveManager.GetInt("ap received owl") == 0)
+            {
+                __instance.gameObject.SetActive(false);
+            }
+        }
+    }
+}
